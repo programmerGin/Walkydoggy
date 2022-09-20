6069f3f761b44f63e675ab57bc647ad76f9122c0
kksshh926 <kksshh926@naver.com>
2022-09-20 20:17:10 +0900

    Initial commit

---
+++ Views/Paging.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Walkydoggy.View
+{
+    /// <summary>
+    /// Paging.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Paging : Page
+    {
+        public Paging()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            Uri uri = new Uri("/Views/Post.xaml", UriKind.Relative);
+            NavigationService.Navigate(uri);
+        }
+
+        private void membersDataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Uri uri = new Uri("/Views/Chat.xaml", UriKind.Relative);
+            NavigationService.Navigate(uri);
+        }
+    }
+}
